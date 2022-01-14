2291c73a0542954be58b036ee5bdb0a88430cd40
natabagus68 <natabagusarisandi68@gmail.com>
2022-01-15 01:14:08 +0700

    crate todolistSlice

---
+++ src/redux/feature/feature.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const todoListSlice = createSlice({
+    name:'todo',
+    initialState:{
+
+    },
+    reducer:{
+
+    }
+})
+
+
+export const {} = todoListSlice.actions
+export default todoListSlice.reducer
